e83febe1e375081d53a5fef27d110c0d4f76ca3d
KoichiroShizuya <bubbler018@gmail.com>
2024-12-07 22:40:41 +0900

    change: 細かいランキング画面のデザイン修正

--- components/RankingDescription.tsx
+++ components/RankingDescription.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const RankingDescription: FC = () => {
   return (
     <div className="flex flex-col items-center border-b border-dashed border-b-yellow-400 pb-4">
-      <Image className="mx-auto" src="/crown.svg" alt="王冠の画像" width={80} height={80} />
+      <Image className="mx-auto mt-4" src="/crown.svg" alt="王冠の画像" width={70} height={70} />
       <h1 className="text-3xl ">ランキング</h1>
       <p className="text-xs mt-2">ランキングに記録が追加されるのは初回の挑戦だけです🙇</p>
     </div>
